5b6c5eed817f3e463dfe113423606344c9d08725
Enocho1-1 <73598581+Enocho1-1@users.noreply.github.com>
2023-02-09 13:30:02 -0500

    sidemenu component

--- ghana/src/App.js
+++ ghana/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css"
 import { Navbar } from "./components/Navbar"
+import { Sidemenu } from "./components/Sidemenu"
 
 export const App = () => {
   return (
       <main>
+        {/* Navagation Button */}
         < Navbar />
+        {/* Navagation Menu */}
+        < Sidemenu />
       </main>
   )
 }
---
+++ ghana/src/components/Sidemenu.js
@@ -0,0 +1,16 @@
+import "./sidemenu.css"
+
+
+export const Sidemenu = () => {
+  return (
+    <div id="menu" className="side-menu">
+      <a href="#" className="modal-close">&times;</a>
+      <div className="menu-options">
+        <a href="#" target="_blank">History</a>
+        <a href="#" target="_blank">Culture</a>
+        <a href="#" target="_blank">Politics</a>
+      </div>
+    </div>
+  )
+}
+
