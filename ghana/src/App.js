4fe0fa8384fd5ee3f82a00d2a81d1c146e3f6095
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2023-02-10 22:46:17 -0500

    video background
    -added video background component

--- ghana/src/App.js
+++ ghana/src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import { Navbar } from "./components/Navbar"
 import { Sidemenu } from "./components/Sidemenu"
 import { Introtext } from "./components/Introtext"
+import { Video } from "./components/Video"
 
 export const App = () => {
   return (
@@ -12,6 +13,8 @@ export const App = () => {
         < Sidemenu />
         {/* Introduction Text Menu */}
         < Introtext />
+        {/* Video Background*/}
+        < Video />
       </main>
   )
 }
---
+++ ghana/src/components/Video.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./video.css"
+import videoBg from "../assests/video-1.mp4"
+
+export const Video = () => {
+  return (
+        <div className="video-back">
+            <video src={videoBg} autoPlay loop muted />
+        </div>
+
+  )
+}
