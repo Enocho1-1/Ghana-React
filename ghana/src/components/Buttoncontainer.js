44e9b918e47455be0dba15ee0943debd56fcb817
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2023-02-25 19:54:50 -0500

    history page

--- ghana/src/components/Buttoncontainer.js
+++ ghana/src/components/Buttoncontainer.js
@@ -1,23 +1,30 @@
-
+import { Button } from "./Button"
+import "./button.css"
 
 export const Buttoncontainer = () => {
     const container = {
         display: "flex",
         flexDirection: "row",
+        justifyContent:"space-evenly",
         position: "absolute",
         top: "25%",
-        left: "50%",
+        left: "45%",
         transform: "translate(-40%, -40%)",
         flexDirection: "row",
-        backgroundColor: "red",
-        height: "125px",
-        width: "125px",
         zIndex: "2",
         marginTop: "12.5em"
     }
   return (
     <div style={container}>
-
+        <Button topic="early-history">
+            <h1>Early History</h1>
+        </Button>
+        <Button topic="independence">
+            <h1>Independence</h1>
+        </Button>
+        <Button topic="present">
+            <h1>Present</h1>
+        </Button>
     </div>
   )
 }
