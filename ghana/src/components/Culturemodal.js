a549db2ecf2163225cdd01ea8936499454d580f3
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2023-02-28 12:54:36 -0500

    Update Culturemodal.js

--- ghana/src/components/Culturemodal.js
+++ ghana/src/components/Culturemodal.js
@@ -4,7 +4,14 @@ export const Culturemodal = () => {
   return (
     <>
         <Modal>
-        
+            {/* Cultural Environment Section */}
+            <section>
+                <h1 className="explore-head">Cultural Environment</h1> 
+                <hr />
+                <div className="explore-text">
+                <p>Ghana has a rich indigenous culture. Culturally, the peoples of Ghana have many affinities with their French-speaking neighbours, but each ethnic group has nuanced cultural attributes. In all parts of the country the cultural heritage is closely linked with religion and the institution of chieftaincy. Various festivals and rites are centred on chieftaincy and the family and are occasioned by such events as harvest, marriage, birth, puberty, and death.</p>
+                </div>
+            </section>
         </Modal>
     </>
    
