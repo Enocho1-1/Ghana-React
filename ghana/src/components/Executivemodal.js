13c5da16fa61ad60c16633996d7e15cd6e5c2049
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2023-03-04 18:29:42 -0500

    Executive Branch Modal Component Edit

--- ghana/src/components/Executivemodal.js
+++ ghana/src/components/Executivemodal.js
@@ -1,7 +1,56 @@
+import { useState } from "react"
 import { Modal } from "./Modal"
-
+import NPP from "../assests/npp.png"
 export const Executivemodal = () => {
+  const executive_branch = [
+    {
+        office:'President',
+        name: 'Nana Akufo-Addo',
+        party:'New Patriotic Party',
+        term:'7 January 2017',
+        image: 'http://127.0.0.1:5501/Ghana/imgs/Nana_Akufo_Addo,_Jan._2020.jpg',
+        party_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/NewPatrioticParty_logo.svg/1200px-NewPatrioticParty_logo.svg.png'
+    },
+    {
+        office:'Vice President',
+        name: 'Mahamudu Bawumia',
+        party:'New Patriotic Party',
+        term:'7 January 2017',
+        image:'http://127.0.0.1:5501/Ghana/imgs/Mahamudu-Bawumia.jpg',
+        party_image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/NewPatrioticParty_logo.svg/1200px-NewPatrioticParty_logo.svg.png'
+    }
+]
+const [executives, setExecutives] = useState(executive_branch)
   return (
-    <Modal></Modal>
+    <Modal>
+        {/* Executive Section */}
+        <section>
+            <h1 className="explore-head"> Executive Branch </h1> 
+            <hr />
+            <table>
+                <tr>
+                  <th>Office</th>
+                  <th>Name</th>
+                  <th>Portrait</th>
+                  <th>Party</th>
+                  <th>Term</th>
+                </tr>
+               
+                 {executives.map( item => (
+                  <tr>
+                    <td>{item.office}</td>
+                    <td>{item.name}</td>
+                    <td><img src={item.image} alt="" /></td>
+                    <td><a href="#" class="inner-link">{item.party}</a><img src={item.party_image} class="hide"/></td>
+                    <td>{item.term}</td>
+                  </tr>
+                 ))}
+              
+            </table>
+            <div className="explore-text">
+              <p> Nana Akufo-Addo holds the office of Presidency, in tandem with his Council of State. The president is the head of state, head of government, and commander in chief of the armed forces. He also selects the vice president. According to the Constitution, more than half of the presidentially appointed ministers of state must be selected from among members of Parliament.</p>
+            </div>
+        </section>
+    </Modal>
   )
 }
