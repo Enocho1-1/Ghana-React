1f6718b4f4e579bae6d3ab7cad379cd21c0e5c72
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2023-03-25 12:57:52 -0400

    Update Partiesmodal.js

--- ghana/src/components/Partiesmodal.js
+++ ghana/src/components/Partiesmodal.js
@@ -24,7 +24,7 @@ export const Partiesmodal = () => {
             <hr />
             <div className="explore-text">
             <img src={NDC} alt="NDC" style={{float:"right"}} title="NDC"/>
-            <p>The National Democratic Congress (NDC) is a social democratic political party in Ghana, founded by <a href="#" className="inner-link">Jerry Rawlings</a><img src={Rawlings}class="hide"/>, who served as Head of State of Ghana from 1981 to 1993 and the President of Ghana from 1993 to 2001. Following the formation of the Provisional National Defence Council (PNDC), which ruled Ghana following the military coup d'état of <a href="#" className="inner-link">Ignatius Kutu Acheampong's</a><img src={Ignatius} class="hide"/> government on 31 December 1981. Consequently, there was pressure from the international community to restore democracy. The NDC was conceived as the ruling party ahead of elections in 1992, in which Rawlings was elected president, and in 1996 Rawlings was re-elected as the NDC candidate. Rawlings' second term ended in 2001.</p>
+            <p>The National Democratic Congress (NDC) is a social democratic political party in Ghana, founded by <a href="#" className="inner-link">Jerry Rawlings</a><img src={Rawlings} className="hide"/>, who served as Head of State of Ghana from 1981 to 1993 and the President of Ghana from 1993 to 2001. Following the formation of the Provisional National Defence Council (PNDC), which ruled Ghana following the military coup d'état of <a href="#" className="inner-link">Ignatius Kutu Acheampong's</a><img src={Ignatius} className="hide"/> government on 31 December 1981. Consequently, there was pressure from the international community to restore democracy. The NDC was conceived as the ruling party ahead of elections in 1992, in which Rawlings was elected president, and in 1996 Rawlings was re-elected as the NDC candidate. Rawlings' second term ended in 2001.</p>
             </div>
         </section>
          {/* New Patriotic Party Section */}
@@ -33,7 +33,7 @@ export const Partiesmodal = () => {
             <hr />
             <div className="explore-text">
             <img src={NPP} alt="NPP" style={{float:"left"}} title="NPP"/>
-            <p>The New Patriotic Party (NPP) is a center-right and liberal-conservative political party in Ghana. Since the democratization of Ghana in 1992, it has been one of the two dominant parties in Ghanaian politics, with its primary rival being the National Democratic Congress (NDC). <a href="#" className="inner-link">John Kufuor</a><img src={Kufor} class="hide"/> of the NPP was President of Ghana from 2001 to 2009 following the Rawlings Administration (NDC). In Ghana presidential terms last for 4 years with a maximum of two terms that can be served, thus Kufor was re-elected on 7 December 2004, and the NPP won 129 out of 230 seats.</p>
+            <p>The New Patriotic Party (NPP) is a center-right and liberal-conservative political party in Ghana. Since the democratization of Ghana in 1992, it has been one of the two dominant parties in Ghanaian politics, with its primary rival being the National Democratic Congress (NDC). <a href="#" className="inner-link">John Kufuor</a><img src={Kufor} className="hide"/> of the NPP was President of Ghana from 2001 to 2009 following the Rawlings Administration (NDC). In Ghana presidential terms last for 4 years with a maximum of two terms that can be served, thus Kufor was re-elected on 7 December 2004, and the NPP won 129 out of 230 seats.</p>
             </div>
         </section>
     </Modal>
