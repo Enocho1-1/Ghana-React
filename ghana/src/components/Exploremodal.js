8360ca430e82a4ff216cea1b052199d125ec8171
Enoch Okyere <73598581+Enocho1-1@users.noreply.github.com>
2023-03-25 15:09:16 -0400

    Update Exploremodal.js

--- ghana/src/components/Exploremodal.js
+++ ghana/src/components/Exploremodal.js
@@ -14,7 +14,7 @@ export const Exploremodal = () => {
       <Modal>
           <h1 className="explore-head">Welcome To Ghana</h1>
           <div className="explore-text">
-              Ghana (The Gold Coast) officially known as the Republic of Ghana is a country in West Africa. Spanning the Gulf of Guinea and the Atlantic Ocean to the south, sharing borders with the Ivory Coast in the west, Burkina Faso in the north, and Togo in the east. With over 31 million people, Ghana is the second-most populous country in West Africa, after Nigeria. The capital and largest city is Accra; other major cities include Kumasi, Tamale, and Sekondi-Takoradi.  
+            <p> Ghana (The Gold Coast) officially known as the Republic of Ghana is a country in West Africa. Spanning the Gulf of Guinea and the Atlantic Ocean to the south, sharing borders with the Ivory Coast in the west, Burkina Faso in the north, and Togo in the east. With over 31 million people, Ghana is the second-most populous country in West Africa, after Nigeria. The capital and largest city is Accra; other major cities include Kumasi, Tamale, and Sekondi-Takoradi.  </p>
           </div>
           <h1 className="explore-head">Famous Ghana Landmarks</h1>
           <div className="landmarks">
